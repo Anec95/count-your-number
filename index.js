0baffefb31e549666dd55396cfbcc5d7023e57ba
Anec95 <miori.thomas95@gmail.com>
2022-10-29 01:01:26 +0200

    new button push

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let plus = document.getElementById("buttonPlus");
+let plus10 = document.getElementById("plusTen");
+let plus100 = document.getElementById("plusHundred");
 let counter = document.getElementById("numberContainer");
 let reset = document.getElementById("buttonReset");
-let minus = document.getElementById("buttonMinus")
+let minus = document.getElementById("buttonMinus");
+let minus10 = document.getElementById("minusTen");
+let minus100 = document.getElementById("minusHundred");
+
 
 counter.textContent = 0;
 
@@ -10,15 +15,25 @@ reset.onclick = function() {
 }
 
 plus.onclick = function() {
-    plus.style.filter = "brightness: 150%;";
     counter.textContent = Number(counter.textContent) + 1;
+};
+
+plus10.onclick = function() {
+    counter.textContent = Number(counter.textContent) + 10;
+};
+
+plus100.onclick = function() {
+    counter.textContent = Number(counter.textContent) + 100;
 };
 
 minus.onclick = function() {
     counter.textContent = Number(counter.textContent) - 1;
 };
 
+minus10.onclick = function() {
+    counter.textContent = Number(counter.textContent) - 10;
+};
 
-
-
-
+minus100.onclick = function() {
+    counter.textContent = Number(counter.textContent) - 100;
+};
